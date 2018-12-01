0242f43eb29b52b5742fc0a85f6065b37398df7a
sureshsvs84 <suresh.s@smnetserv.com>
2018-12-01 23:39:40 +0530

    build file

---
+++ precache-manifest.2b6f5c87d8be3208221e2a27eb5d860b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sureshsvs84/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "624add0b2a150b120d9d",
+    "url": "/sureshsvs84/hello/static/js/runtime~main.624add0b.js"
+  },
+  {
+    "revision": "d977d54dbcbe12fe1681",
+    "url": "/sureshsvs84/hello/static/js/main.d977d54d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/sureshsvs84/hello/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "d977d54dbcbe12fe1681",
+    "url": "/sureshsvs84/hello/static/css/main.0e8167ec.chunk.css"
+  },
+  {
+    "revision": "8576af189906badad7061d22d7175582",
+    "url": "/sureshsvs84/hello/index.html"
+  }
+];
